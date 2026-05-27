78c794ecb442535e8f2c0d6f478c3d5876af5639
brvinxdvnce <ivanovdanil.hits2024@gmail.com>
2026-05-28 00:44:37 +0300

    register GrpcErrorHandler and gRPC clients

---
+++ src/phoenix-web-bff.Presentation/DependencyInjection/GrpcClientsRegisterExtension.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using Phoenix;
+
+namespace phoenix_web_bff.Presentation.DependencyInjection;
+
+public static class GrpcClientsRegisterExtension
+{
+    public static WebApplicationBuilder AddGrpcClients(this WebApplicationBuilder builder)
+    {
+        var coreChannel = GrpcChannel.ForAddress(builder.Configuration["Grpc:CoreAddress"]!);
+        var sentryChannel = GrpcChannel.ForAddress(builder.Configuration["Grpc:SentryAddress"]!);
+
+        builder.Services.AddSingleton(new IncidentService.IncidentServiceClient(coreChannel));
+        builder.Services.AddSingleton(new UnitService.UnitServiceClient(coreChannel));
+        builder.Services.AddSingleton(new MetricService.MetricServiceClient(sentryChannel));
+        
+        return builder;
+    }
+}
