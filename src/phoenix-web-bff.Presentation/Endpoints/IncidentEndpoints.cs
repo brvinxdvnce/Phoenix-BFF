03782da84c12a3b780763f91ebdd184070e849a3
brvinxdvnce <ivanovdanil.hits2024@gmail.com>
2026-06-02 21:54:29 +0300

    Added group register endpoint

--- src/phoenix-web-bff.Presentation/Endpoints/IncidentEndpoints.cs
+++ src/phoenix-web-bff.Presentation/Endpoints/IncidentEndpoints.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using phoenix_web_bff.Presentation.Common.ResultUtils.Extensions;
 using phoenix_web_bff.Presentation.Incidents.Commands.CreateIncident;
+using phoenix_web_bff.Presentation.Incidents.Commands.CreateIncidentGroupGroup;
 using phoenix_web_bff.Presentation.Incidents.Queries.GetIncident;
 using phoenix_web_bff.Presentation.Incidents.Queries.GetIncidents;
+using Phoenix;
 
 namespace phoenix_web_bff.Presentation.Endpoints;
 
@@ -14,25 +16,32 @@ public static class IncidentEndpoints
         var group = app.MapGroup("api/v1/incidents");
 
         group.MapPost("/", async (
-                    [FromBody] CreateIncidentCommand cmd, 
+                    [FromBody] CreateIncidentCommand cmd,
                     [FromServices] IMediator mediator,
-                    CancellationToken ct) => 
+                    CancellationToken ct) =>
                 (await mediator.Send(cmd, ct)).ToActionResult())
-            .WithName("Создать инцидент");
-        
-        group.MapGet ("/", async (
+            .WithName("Add incident");
+
+        group.MapGet("/", async (
                     [FromServices] IMediator mediator,
-                    CancellationToken ct) => 
+                    CancellationToken ct) =>
                 (await mediator.Send(new GetIncidentsQuery(), ct)).ToActionResult())
-            .WithName("Просмотреть инциденты");
-        
-        group.MapGet ("/{incidentId}", async (
+            .WithName("Get incidents");
+
+        group.MapGet("/{incidentId}", async (
                     [FromRoute] string incidentId,
                     [FromServices] IMediator mediator,
-                    CancellationToken ct) => 
+                    CancellationToken ct) =>
                 (await mediator.Send(new GetIncidentQuery(incidentId), ct)).ToActionResult())
-            .WithName("Просмотреть один инцидент");
-        
-        return app;
+            .WithName("Get incident by Id");
+
+        group.MapPost("/groups", async (
+                    [FromBody] CreateIncidentGroupRequest request,
+                    [FromServices] IMediator mediator,
+                    CancellationToken ct) =>
+                (await mediator.Send(new CreateIncidentGroupCommand(request), ct)).ToActionResult())
+            .WithName("Create incident group");
+            
+    return app;
     }
 }
